115d5800220f6ffffd7e10e31b04aef14c837650
VipinKumawat <64091048+VipinKumawat@users.noreply.github.com>
2025-07-29 14:38:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -113,7 +113,7 @@ function renderProductPricing(product) {
   }
 
   let htmlContent = `
-    <h3>Type: ${product.type} Colour: ${product.color} Available Sizes & Pricing:</h3>
+    <h3> Available Sizes & Pricing For  <span style="color: OrangeRed;">${product.color}</span> <span style="color: BlueViolet;">${product.type}</span> Kurta:</h3>
     <div class="tabs">
       <div class="tab-buttons">
         <button class="tab-button active" data-tab="mens">Mens</button>
